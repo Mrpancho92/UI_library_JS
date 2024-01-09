4904ad84d9aee0ddd8d9d2ebe46778b0f12a5a85
Pavel <133679525+Mrpancho92@users.noreply.github.com>
2024-01-09 12:54:53 +0300

    carousel_&_services

--- src/js/main.js
+++ src/js/main.js
@@ -19,58 +19,62 @@ import $ from './lib/lib';
 // $('button').gasRight(2000);
 // console.log($('.more').eq(0).siblings());
 
-$('#first').on('click', () => {
-    $('div').eq(1).fadeToggle(800);
-});
-$('.wrap').html(
-    `<div class="dropdown">
-    <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
-        <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
-            <a href="#" class="dropdown-item">Action</a>
-            <a href="#" class="dropdown-item">Action #2</a>
-            <a href="#" class="dropdown-item">Action #3</a>
-        </div>
-</div>`
-);
-$('.dropdown-toggle').dropdown();
-// $('[data-count="second"]').on('click', () => {
-//     $('div').eq(2).fadeToggle(800);
+// $('#first').on('click', () => {
+//     $('div').eq(1).fadeToggle(800);
 // });
-// $('button').eq(2).on('click', () => {
-//     $('.w-500').fadeToggle(800); 
-// });
+// $('.wrap').html(
+//     `<div class="dropdown">
+//     <button class="btn btn-primary dropdown-toggle" id="dropdownMenuButton">Dropdown button</button>
+//         <div class="dropdown-menu" data-toggle-id="dropdownMenuButton">
+//             <a href="#" class="dropdown-item">Action</a>
+//             <a href="#" class="dropdown-item">Action #2</a>
+//             <a href="#" class="dropdown-item">Action #3</a>
+//         </div>
+// </div>`
+// );
+// $('.dropdown-toggle').dropdown();
+// // $('[data-count="second"]').on('click', () => {
+// //     $('div').eq(2).fadeToggle(800);
+// // });
+// // $('button').eq(2).on('click', () => {
+// //     $('.w-500').fadeToggle(800); 
+// // });
 
-$('#trigger').click(() => {
-    $('#trigger').createModal({
-        text: {
-            title: 'Modal title',
-            body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda totam aperiam dignissimos obcaecati corrupti aut animi illo dolor explicabo autem fugit, sunt culpa quibusdam nam sint est corporis quaerat non.'
-        },
-        btns: {
-            count: 3, 
-            settings: [
-                [
-                    'close',
-                    ['btn-danger', 'mr-10'],
-                    true
-                ],
-                [
-                    'save changes',
-                    ['btn-success'],
-                    false,
-                    () => {
-                        alert('данные сохранены');
-                    }
-                ],
-                [
-                    'Another btn',
-                    ['btn-warning', 'ml-10'],
-                    false,
-                    () => {
-                        alert('hello world');
-                    }
-                ]
-            ]
-        }
-    });
-})
+// $('#trigger').click(() => {
+//     $('#trigger').createModal({
+//         text: {
+//             title: 'Modal title',
+//             body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda totam aperiam dignissimos obcaecati corrupti aut animi illo dolor explicabo autem fugit, sunt culpa quibusdam nam sint est corporis quaerat non.'
+//         },
+//         btns: {
+//             count: 3, 
+//             settings: [
+//                 [
+//                     'close',
+//                     ['btn-danger', 'mr-10'],
+//                     true
+//                 ],
+//                 [
+//                     'save changes',
+//                     ['btn-success'],
+//                     false,
+//                     () => {
+//                         alert('данные сохранены');
+//                     }
+//                 ],
+//                 [
+//                     'Another btn',
+//                     ['btn-warning', 'ml-10'],
+//                     false,
+//                     () => {
+//                         alert('hello world');
+//                     }
+//                 ]
+//             ]
+//         }
+//     });
+// })
+
+
+// $().get('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(res => console.log(res));
